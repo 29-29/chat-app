cfcce0371685de028d15b8ef7b26087ea614bce6
29-29 <ackx29sodo@gmail.com>
2024-11-08 07:37:37 +0800

    added user details on toolbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,18 +1,29 @@
 <script setup lang="ts">
 import { Auth, getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
-import { onMounted, ref } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
-
 const isLoggedIn = ref(false);
-
 let auth: Auth;
+const userData = reactive({
+  name: '',
+  photoURL: '',
+});
+const userLoading = ref(true);
+
+const fetchUserData = async () => {
+  userData.name = <string>auth.currentUser?.displayName;
+  userData.photoURL = <string>auth.currentUser?.photoURL;
+  userLoading.value = false;
+};
+
 onMounted(() => {
   auth = getAuth();
   onAuthStateChanged(auth, (user) => {
     if (user) {
       isLoggedIn.value = true;
+      fetchUserData();
     } else {
       isLoggedIn.value = false;
     }
@@ -31,7 +42,15 @@ const handleSignOut = () => {
     <q-header>
       <q-toolbar class="bg-pink-5">
         <q-toolbar-title>Ahmad Chat App</q-toolbar-title>
-
+        <div v-if="userLoading" class="q-px-md">
+          <q-skeleton type="rect" height="30px" width="150px" />
+        </div>
+        <div v-else class="q-mx-sm">
+          {{ userData.name }}
+          <q-avatar size="md" class="q-mx-sm">
+            <img :src="userData.photoURL" />
+          </q-avatar>
+        </div>
         <div>
           <q-btn
             label="sign out"

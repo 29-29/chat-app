a04bf9e6d723c91b957fcd9731db35d17c20f838
29-29 <ackx29sodo@gmail.com>
2024-11-08 07:34:52 +0800

    reconfigured firebase and firestore

--- src/boot/firebase.ts
+++ src/boot/firebase.ts
@@ -1,4 +1,5 @@
-import { initializeApp } from 'firebase/app';
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 // import { getFirestore } from 'firebase/firestore';
 import { boot } from 'quasar/wrappers';
 // import { VueFire, VueFireAuth } from 'vuefire';
@@ -36,14 +37,15 @@ import { boot } from 'quasar/wrappers';
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async () => {
-  initializeApp({
-    apiKey: 'AIzaSyCT4tQBRSL-dN_5DbEoIn-fwL3Y5NSZEsc',
-    authDomain: 'ahmad-chat-app-f1122.firebaseapp.com',
-    projectId: 'ahmad-chat-app-f1122',
-    storageBucket: 'ahmad-chat-app-f1122.firebasestorage.app',
-    messagingSenderId: '389289109657',
-    appId: '1:389289109657:web:108810f32e48bc83b63008',
-    measurementId: 'G-WCLYN8BQ3J',
-  });
+export const firebaseApp: FirebaseApp = initializeApp({
+  apiKey: 'AIzaSyCT4tQBRSL-dN_5DbEoIn-fwL3Y5NSZEsc',
+  authDomain: 'ahmad-chat-app-f1122.firebaseapp.com',
+  projectId: 'ahmad-chat-app-f1122',
+  storageBucket: 'ahmad-chat-app-f1122.firebasestorage.app',
+  messagingSenderId: '389289109657',
+  appId: '1:389289109657:web:108810f32e48bc83b63008',
+  measurementId: 'G-WCLYN8BQ3J',
 });
+export default boot(async () => {});
+
+export const db = getFirestore(firebaseApp);

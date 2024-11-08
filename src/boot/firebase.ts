00fa4477a73c8e251fa3625feda5b2c9a3698184
29-29 <ackx29sodo@gmail.com>
2024-11-08 11:09:42 +0800

    refactor: centralize collection references

--- src/boot/firebase.ts
+++ src/boot/firebase.ts
@@ -1,39 +1,7 @@
 import { FirebaseApp, initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-// import { getFirestore } from 'firebase/firestore';
+import { collection, getFirestore } from 'firebase/firestore';
 import { boot } from 'quasar/wrappers';
-// import { VueFire, VueFireAuth } from 'vuefire';
-// import { computed, onUnmounted, ref } from 'vue';
-// import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-
-// const firebaseApp = initializeApp({
-//   apiKey: 'AIzaSyCT4tQBRSL-dN_5DbEoIn-fwL3Y5NSZEsc',
-//   authDomain: 'ahmad-chat-app-f1122.firebaseapp.com',
-//   projectId: 'ahmad-chat-app-f1122',
-//   storageBucket: 'ahmad-chat-app-f1122.firebasestorage.app',
-//   messagingSenderId: '389289109657',
-//   appId: '1:389289109657:web:108810f32e48bc83b63008',
-//   measurementId: 'G-WCLYN8BQ3J',
-// });
-
-// export const db = getFirestore(firebaseApp);
-// export const auth = getAuth(firebaseApp);
-
-// export function useAuth() {
-//   const user = ref(null);
-//   const unsubscribe = auth.onAuthStateChanged((_user) => (user.value = _user));
-//   onUnmounted(unsubscribe);
-//   const isLogin = computed(() => user.value !== null);
-
-//   const signIn = async () => {
-//     const googleProvider = new GoogleAuthProvider();
-//     await signInWithPopup(auth, googleProvider);
-//   };
-
-//   const signOut = () => auth?.signOut();
-
-//   return { user, isLogin, signIn, signOut };
-// }
+//// import admin from 'firebase-admin';
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
@@ -46,6 +14,17 @@ export const firebaseApp: FirebaseApp = initializeApp({
   appId: '1:389289109657:web:108810f32e48bc83b63008',
   measurementId: 'G-WCLYN8BQ3J',
 });
+//// export const serviceAccount = require(`../../${process.env.FIREBASE_ADMIN_SDK}`);
+
+//// admin.initializeApp({
+////   credential: admin.credential.cert(serviceAccount),
+//// });
+
 export default boot(async () => {});
 
 export const db = getFirestore(firebaseApp);
+export const chatroomsCol = collection(db, 'chatrooms');
+export const usersCol = collection(db, 'users');
+export const messagesCol = (id: string) => {
+  return collection(db, `chatrooms/${id}/messages`);
+};

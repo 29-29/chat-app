d43fde352d16fc740f67971bdf4f0ae03048b049
29-29 <ackx29sodo@gmail.com>
2024-11-15 19:08:24 +0800

    feat: added the scroll to bottom button

--- src/components/ChatMessageList.vue
+++ src/components/ChatMessageList.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-import { ref, defineProps } from 'vue';
+import { ref, defineProps, onMounted, computed } from 'vue';
 import { MessageData } from './ChatMessage.vue';
 import ChatMessage from './ChatMessage.vue';
+import { QScrollArea, scroll } from 'quasar';
+const {
+  // getVerticalScrollPosition,
+  // setVerticalScrollPosition,
+  // getScrollHeight,
+} = scroll;
 
 const props = defineProps<{
   messages: {
@@ -10,12 +16,33 @@ const props = defineProps<{
   }[];
 }>();
 
-const scrollArea = ref<Element | null>(null);
+const scrollAreaRef = ref<QScrollArea | null>(null);
+const scrollHeight = computed(
+  () => scrollAreaRef.value?.getScrollTarget().scrollHeight as number
+);
+const clientHeight = computed(
+  () => scrollAreaRef.value?.getScrollTarget().clientHeight as number
+);
+const scrollPercentage = computed(
+  () => scrollAreaRef.value?.getScrollPercentage().top
+);
+const showToBottomBtn = computed(() => {
+  return scrollHeight.value > clientHeight.value && scrollPercentage.value != 1;
+});
+const scrollToBottom = () => {
+  scrollAreaRef.value?.setScrollPercentage('vertical', 1, 300);
+};
+
+onMounted(() => {
+  scrollAreaRef.value?.setScrollPercentage('vertical', 1, 300);
+  setTimeout(() => {}, 300);
+  scrollToBottom();
+});
 </script>
 
 <template>
   <q-card flat bordered class="messages-container rounded-borders">
-    <q-scroll-area ref="scrollArea" style="height: inherit">
+    <q-scroll-area ref="scrollAreaRef" style="height: inherit">
       <q-list dense class="full-height" ref="list">
         <div
           v-if="props.messages.length === 0"
@@ -32,6 +59,16 @@ const scrollArea = ref<Element | null>(null);
           <ChatMessage :data="message.data" />
         </q-item>
       </q-list>
+      <TransitionGroup name="toBottomBtn">
+        <q-page-sticky v-if="showToBottomBtn" position="bottom" class="q-mb-md">
+          <q-btn
+            color="pink-5"
+            @click="scrollToBottom"
+            :label="scrollPercentage"
+            dense
+          />
+        </q-page-sticky>
+      </TransitionGroup>
     </q-scroll-area>
   </q-card>
 </template>
@@ -39,6 +76,13 @@ const scrollArea = ref<Element | null>(null);
 <style lang="sass" scoped>
 .messages-container
   border:1px solid $pink-5
-.q-item
+.q-item:not(:last-child)
   border-bottom: 1px solid $pink-11
+.toBottomBtn-enter-active,
+.toBottomBtn-leave-active
+  transition: all $animate-duration ease
+.toBottomBtn-enter-from,
+.toBottomBtn-leave-to
+  opacity: 0
+  transform: translateX(16px)
 </style>

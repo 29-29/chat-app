b70d5f0cd5421bef65924b863317bd8d238fc886
29-29 <ackx29sodo@gmail.com>
2024-11-16 22:12:21 +0800

    feat(public-chatrooms): separated public and private chatrooms
    - added "private" and "public" props to ChatList component
    - changed how subscriptions to chatrooms collection is done

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-import { doc, onSnapshot, getDoc, query, where } from 'firebase/firestore';
+import { doc, onSnapshot, query, where, getDoc } from 'firebase/firestore';
 import { onMounted, ref, onUnmounted, computed } from 'vue';
 import ChatListItem from './ChatListItem.vue';
 import { db, chatroomsCol } from 'src/boot/firebase';
 import { user } from 'src/composables/auth';
+
+const props = defineProps({
+  private: {
+    default: false,
+    type: Boolean,
+  },
+  public: {
+    default: false,
+    type: Boolean,
+  },
+});
 
 const listLoading = ref(true);
 const userChatrooms = ref<Array<{ id: string; lastMessageTime: number }>>([]);
@@ -17,21 +28,21 @@ const subscribeToUserRooms = async () => {
   unsubscribeUser = onSnapshot(
     doc(db, 'users', user.value.uid),
     async (userDoc) => {
-      const roomIds = userDoc.data()?.rooms || [];
-
-      const roomsWithTimestamp = await Promise.all(
-        roomIds.map(async (roomId: string) => {
-          const roomDoc = await getDoc(doc(chatroomsCol, roomId));
-          const roomData = roomDoc.data();
-          return {
-            id: roomId,
-            lastMessageTime: roomData?.lastMessageTime?.toMillis() || 0,
-          };
-        })
-      );
-
-      userChatrooms.value = roomsWithTimestamp;
-      listLoading.value = false;
+      const roomIds: Array<string> = userDoc.data()?.rooms || [];
+      const promises = roomIds.map((roomId) => {
+        const roomRef = getDoc(doc(db, 'chatrooms', roomId));
+        return roomRef;
+      });
+      Promise.all(promises).then((docs) => {
+        docs.forEach((doc) => {
+          const roomData = doc.data();
+          if (roomData?.private)
+            userChatrooms.value.push({
+              id: doc.id,
+              lastMessageTime: roomData?.lastMessageTime?.toMillis() || 0,
+            });
+        });
+      });
     }
   );
 };
@@ -40,10 +51,13 @@ const subscribeToPublicRooms = () => {
   const publicRoomsQuery = query(chatroomsCol, where('private', '==', false));
 
   unsubscribePublic = onSnapshot(publicRoomsQuery, async (snapshot) => {
-    const publicRoomsData = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      lastMessageTime: doc.data().lastMessageTime?.toMillis() || 0,
-    }));
+    const publicRoomsData = snapshot.docs.map((doc) => {
+      const docData = doc.data();
+      return {
+        id: doc.id,
+        lastMessageTime: docData?.lastMessageTime?.toMillis() || 0,
+      };
+    });
     publicRooms.value = publicRoomsData;
   });
 };
@@ -63,8 +77,13 @@ const allRooms = computed(() => {
 });
 
 onMounted(async () => {
-  await subscribeToUserRooms();
-  subscribeToPublicRooms();
+  if (props.public && !props.private) subscribeToPublicRooms();
+  else if (props.private && !props.public) await subscribeToUserRooms();
+  else {
+    subscribeToPublicRooms();
+    subscribeToUserRooms();
+  }
+  listLoading.value = false;
 });
 
 onUnmounted(() => {
@@ -84,12 +103,24 @@ onUnmounted(() => {
       </q-item>
     </template>
     <template v-else>
-      <q-item v-if="allRooms.length === 0">
+      <q-item
+        v-if="
+          (props.private
+            ? userChatrooms.length
+            : props.public
+            ? publicRooms.length
+            : allRooms.length) === 0
+        "
+      >
         <q-item-section>No chatrooms available.</q-item-section>
       </q-item>
       <ChatListItem
         v-else
-        v-for="room in allRooms"
+        v-for="room in props.public
+          ? publicRooms
+          : props.private
+          ? userChatrooms
+          : allRooms"
         :key="room.id"
         :id="room.id"
         :max-length="100"

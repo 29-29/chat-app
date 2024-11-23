2b2b428b17d56997f6f2f8fe06d2bc49f036323f
29-29 <ackx29sodo@gmail.com>
2024-11-24 01:45:12 +0800

    feat: created Avatar component

---
+++ src/components/AvatarComp.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps({
+  url: {
+    type: String,
+    default: '',
+  },
+});
+
+const loadIcon = ref(false);
+</script>
+<template>
+  <q-icon
+    v-if="loadIcon || url == ''"
+    name="account_circle"
+    color="white"
+    size="md"
+  />
+  <q-avatar v-else>
+    <q-img :src="props.url" @error="loadIcon = true" />
+  </q-avatar>
+</template>
